a023e8bfce43386874a40e917c9ed611314bacc8
Thomas Conrad <thomas@figurement.com>
2024-12-10 09:01:27 +0100

    day 10

---
+++ src/bin/10.rs
@@ -0,0 +1,121 @@
+use std::collections::HashMap;
+
+use advent_of_code::helpers::get_safe;
+
+fn parse(input: &str) -> (Vec<Vec<u8>>, Vec<[isize; 2]>) {
+    let mut trailheads = Vec::new();
+    (
+        input
+            .lines()
+            .enumerate()
+            .map(|(y, line)| {
+                line.chars()
+                    .enumerate()
+                    .map(|(x, c)| {
+                        if c == '0' {
+                            trailheads.push([x as isize, y as isize]);
+                        }
+                        c as u8 - b'0'
+                    })
+                    .collect()
+            })
+            .collect(),
+        trailheads,
+    )
+}
+
+fn get_neighbors(map: &Vec<Vec<u8>>, [x, y]: &[isize; 2], height: u8) -> Vec<[isize; 2]> {
+    [[-1, 0], [1, 0], [0, -1], [0, 1]]
+        .iter()
+        .filter_map(|[dx, dy]| {
+            get_safe([x + dx, y + dy], map)
+                .filter(|&&c| c == height + 1)
+                .map(|_| [x + dx, y + dy])
+        })
+        .collect()
+}
+
+fn hike<'a>(
+    map: &Vec<Vec<u8>>,
+    pos: impl Iterator<Item = (&'a [isize; 2], &'a u32)>,
+    height: u8,
+) -> Vec<([isize; 2], u32)> {
+    if height == 9 {
+        return pos.map(|(p, m)| (*p, *m)).collect();
+    }
+
+    let pos = pos
+        .flat_map(|(p, mult)| {
+            get_neighbors(map, p, height)
+                .into_iter()
+                .map(move |n| (n, *mult))
+        })
+        .fold(HashMap::new(), |mut acc, (p, mult)| {
+            let m = acc.entry(p).or_insert(0);
+            *m += mult;
+
+            acc
+        });
+    return hike(map, pos.iter(), height + 1);
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let (map, trailheads) = parse(input);
+    Some(
+        trailheads
+            .iter()
+            .map(|head| hike(&map, [(head, &1)].into_iter(), 0).len() as u32)
+            .sum(),
+    )
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    let (map, trailheads) = parse(input);
+    Some(
+        trailheads
+            .iter()
+            .map(|head| {
+                hike(&map, [(head, &1)].into_iter(), 0)
+                    .iter()
+                    .map(|(_, v)| *v)
+                    .sum::<u32>() as u32
+            })
+            .sum(),
+    )
+}
+
+#[cfg(feature = "solve")]
+fn main() {
+    let input = &advent_of_code::read_file("inputs", 10);
+    advent_of_code::solve!(1, part_one, input);
+    advent_of_code::solve!(2, part_two, input);
+}
+
+#[cfg(feature = "submit1")]
+fn main() {
+    let input = &advent_of_code::read_file("inputs", 10);
+    advent_of_code::submit::submit(10, 1, part_one, input);
+}
+
+#[cfg(feature = "submit2")]
+fn main() {
+    let input = &advent_of_code::read_file("inputs", 10);
+    advent_of_code::submit::submit(10, 2, part_two, input);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let input = advent_of_code::read_file("examples", 10);
+        assert_eq!(part_one(&input), Some(36));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let input = advent_of_code::read_file("examples", 10);
+        assert_eq!(part_two(&input), Some(81));
+    }
+}
